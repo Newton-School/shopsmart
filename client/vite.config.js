fe64d8f54fe8f3991e81dd6d87bfe26739a862a3
SRB77 <soumyach144@gmail.com>
2026-03-20 13:42:22 +0530

    Change the base configuration and some settings iin github too

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,21 +3,21 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/shopsmart", 
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5001',
-                changeOrigin: true,
-            }
-        }
+  base: "/shopsmart_workflow/",
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+      },
     },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.js',
-        include: ['src/**/*.test.{js,jsx}', 'src/__tests__/**/*.test.{js,jsx}'],
-        exclude: ['node_modules', 'tests/e2e/**'],
-    },
-})
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    include: ["src/**/*.test.{js,jsx}", "src/__tests__/**/*.test.{js,jsx}"],
+    exclude: ["node_modules", "tests/e2e/**"],
+  },
+});
